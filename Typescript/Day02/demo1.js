ce27871aa1d148bdf9cc6193e930d3f0e94c9a65
Raokhande sumit Ashok <raokhande.sumit@gmail.com>
2023-01-10 21:47:50 +0530

    Day02

---
+++ Typescript/Day02/demo1.js
@@ -0,0 +1,28 @@
+// console.log("Hello world....");
+// console.log('Welcome angular 12 Module ')
+// console.log(`
+//         Hopes so u r enjoy....
+// `);
+/** */
+var a1, a2;
+var a_1, a$1;
+//data types 
+//number(int,long,double,float,short),string(""/''),boolean(true/false)
+//any (number,string,boolean)
+// void => it is neutral data type => it does not point anything
+// misec. null & undefined=> it used as data type as well as value 
+var a = 10;
+// console.log("Value of a is "+a);
+// var b!:number;
+// console.log('Value of b is '+b);
+// var str!:string;
+// console.log(` 
+//     Value of str is ${str}
+// `);
+var c;
+c = 2.5;
+console.log('Value of c ' + c);
+c = 'Sumit';
+console.log('Value of c ' + c);
+c = true;
+console.log('Value of c ' + c);
